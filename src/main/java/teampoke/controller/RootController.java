b44514d655b4c9030a0e963ab9d0b20c338d1b47
Ana-Mes <anamesa_98@hotmail.com>
2023-01-20 11:52:24 +0000

    reorganizacion de carpetas y video de fondo para la interfaz

--- src/main/java/teampoke/controller/RootController.java
+++ src/main/java/teampoke/controller/RootController.java
@@ -2,22 +2,32 @@ package teampoke.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class RootController implements Initializable {
 
+	// model
+	
 	// view
 	
 	@FXML
-    private HBox view;
+    private VBox view;
+	
+	@FXML
+    private MediaView mediaView;
+
 	
 	public RootController() {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/StartView.fxml"));
 		loader.setController(this);
 		try {
 			loader.load();
@@ -28,11 +38,23 @@ public class RootController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-
+		
+		// para el video de fondo
+		Media media = new Media(Paths.get("media/fondo.mp4").toUri().toString());
+		MediaPlayer player = new MediaPlayer(media);
+		mediaView.setMediaPlayer(player);
+		player.setVolume(0);
+		player.play();
+		
+		// para que el video sea responsive bindeamos el ancho y la altura del video a las de su contenedor
+		mediaView.fitWidthProperty().bind(view.widthProperty());
+		mediaView.fitHeightProperty().bind(view.heightProperty());
+		mediaView.setPreserveRatio(false); // para que el video pueda deformarse
+		
+		
 	}
 	
-	public HBox getView() {
+	public VBox getView() {
 		return view;
 	}
 
