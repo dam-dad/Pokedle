9f08e8ca61ef198e79ae6665512711006a33a0d4
Ana-Mes <anamesa_98@hotmail.com>
2023-01-26 14:11:52 +0000

    Video de fondo configurado y logo añadido

--- src/main/java/teampoke/controller/RootController.java
+++ src/main/java/teampoke/controller/RootController.java
@@ -8,7 +8,9 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -16,16 +18,18 @@ import javafx.scene.media.MediaView;
 public class RootController implements Initializable {
 
 	// model
-	
-	// view
-	
-	@FXML
-    private VBox view;
-	
-	@FXML
-    private MediaView mediaView;
 
-	
+	// view
+
+	@FXML
+	private StackPane view;
+
+	@FXML
+	private MediaView mediaView;
+
+    @FXML
+    private ImageView logo;
+
 	public RootController() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/StartView.fxml"));
 		loader.setController(this);
@@ -35,27 +39,35 @@ public class RootController implements Initializable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		// para el video de fondo
 		Media media = new Media(Paths.get("media/fondo.mp4").toUri().toString());
 		MediaPlayer player = new MediaPlayer(media);
 		mediaView.setMediaPlayer(player);
+		mediaView.toBack(); // para que el video esté en el fondo
 		player.setVolume(0);
-		player.play();
-		
-		// para que el video sea responsive bindeamos el ancho y la altura del video a las de su contenedor
+		player.setAutoPlay(true);
+		player.setCycleCount(MediaPlayer.INDEFINITE); // para que el video esté en bucle
+
+		// para que el video sea responsive bindeamos el ancho y la altura del video a
+		// los de su contenedor
 		mediaView.fitWidthProperty().bind(view.widthProperty());
 		mediaView.fitHeightProperty().bind(view.heightProperty());
 		mediaView.setPreserveRatio(false); // para que el video pueda deformarse
 		
 		
 	}
-	
-	public VBox getView() {
+
+	public StackPane getView() {
 		return view;
+	}
+
+	@FXML
+	void onMousePressed(MouseEvent event) {
+		System.out.println("click");
 	}
 
 }
